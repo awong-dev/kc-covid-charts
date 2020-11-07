45e923e049a0971072edaf8994a52a62eaac3ff5
Albert J. Wong <awong-dev@gmail.com>
2020-11-06 23:56:52 -0800

    Add basic data table

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,27 @@
 import Head from 'next/head'
+import DataTable from './data-table'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({covidData}) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>King County Covid Charts</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          King County Covid Charts
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Provides a slightly different view of the data on 
+          <a href="https://www.kingcounty.gov/depts/health/covid-19/data/daily-summary.aspx">King County Daily Outbreak Summary.</a>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <DataTable locationType="HRA" covidData={covidData['HRA']} />
         </div>
       </main>
 
@@ -62,4 +37,16 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://firebasestorage.googleapis.com/v0/b/kc-covid-chart.appspot.com/o/processed%2Fdata.json?alt=media&token=18ebf1ec-4143-4272-9b3a-4bee6c62793e');
+  const covidData = await res.json();
+
+  return {
+    props: {
+      covidData,
+    },
+    revalidate: 1, // In seconds
+  }
 }
